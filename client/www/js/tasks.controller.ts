b6198eef2be1486dc7f40826cf52d30481dcdfae
Olof <olof.bendt@transformatordesign.se>
2015-08-16 10:26:56 +0200

    Refactored modal from template to template url.

--- client/www/js/tasks.controller.ts
+++ client/www/js/tasks.controller.ts
@@ -14,31 +14,6 @@ module controllers {
         modal;
         popover;
 
-        private modalTemplate = '' +
-            '<ion-modal-view>' +
-            '<ion-header-bar>' +
-            '<h1 class="title">{{currentTask.content}}</h1>' +
-            '</ion-header-bar>' +
-            '<ion-content>' +
-            '<div class="card list">' +
-            '<div class="item" ng-click="doToday(currentTask)">Do today</div>' +
-            '<div class="item" ng-click="doTomorrow(currentTask)">Do tomorrow</div>' +
-            '<div class="item" ng-click="doNextWeek(currentTask)">Do next week</div>' +
-            '<div class="item" ng-click="doNextMonth(currentTask)">Do next month</div>' +
-            '<div class="item" ng-click="hideModal()">Cancel</div>' +
-            '</div>' +
-            '</ion-content>' +
-            '</ion-modal-view>';
-
-        private popoverTemplate = '' +
-            '<ion-popover-view ng-click="hideModal()">' +
-            '<ion-header-bar>' +
-            '<h1 class="title">Do When?</h1>' +
-            '</ion-header-bar>' +
-            '<ion-content>' +
-            '{{currentTask.content}}' +
-            '</ion-content>' +
-            '</ion-popover-view>';
 
         static $inject = [
             "$scope",
@@ -50,9 +25,12 @@ module controllers {
         constructor(isolateScope:any, taskListService:services.TaskListService, $ionicModal, $ionicPopover) {
             this.name = isolateScope.name;
             this.isolateScope = isolateScope;
-            this.modal = $ionicModal.fromTemplate(this.modalTemplate, {
+
+            $ionicModal.fromTemplateUrl('../templates/modal-actions.html', {
                 scope: isolateScope,
                 animation: 'slide-in-up'
+            }).then((modal) => {
+                this.modal = modal;
             });
 
             this.popover = $ionicPopover.fromTemplate(this.popoverTemplate, {
@@ -66,9 +44,6 @@ module controllers {
             this.isolateScope.doTomorrow = this.doTomorrow.bind(this);
             this.isolateScope.doNextWeek = this.doNextWeek.bind(this);
             this.isolateScope.doNextMonth = this.doNextMonth.bind(this);
-
-            this.isolateScope.showPopover = this.showPopover.bind(this);
-            this.isolateScope.hidePopover = this.hidePopover.bind(this);
 
             this.isolateScope.taskLists = taskListService;
             this.isolateScope.taskLists.refresh();
@@ -118,15 +93,6 @@ module controllers {
             task.due = moment().add(1, 'months').format('YYYY/MM/DD');
             this.isolateScope.taskLists.distributeTasks();
             this.hideModal();
-        }
-
-        showPopover($event:ng.IAngularEvent, listItem:ListItem) {
-            this.isolateScope.currentTask = listItem;
-            this.popover.show($event);
-        }
-
-        hidePopover() {
-            this.popover.hide();
         }
     }
 

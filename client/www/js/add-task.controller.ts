691e9206fa928d8f8ffdbf017a109af41e55aa4b
Olof <olof.bendt@transformatordesign.se>
2015-08-30 22:57:34 +0200

    Adds simple toast messages to add task.

--- client/www/js/add-task.controller.ts
+++ client/www/js/add-task.controller.ts
@@ -8,34 +8,52 @@ module controllers {
     export class AddTaskController {
         tasksService:services.TasksService;
         $log:ng.ILogService;
+        $q:ng.IQService;
         $ionicTabsDelegate;
+        $ionicLoading;
 
         content:string = '';
         due:string = 'asap';
-
 
         static $inject = [
             'TasksService',
             '$log',
             '$q',
-            '$ionicTabsDelegate'
+            '$ionicTabsDelegate',
+            '$ionicLoading'
         ];
 
-        constructor(tasksService:services.TasksService, $log:ng.ILogService, $q:ng.IQService, $ionicTabsDelegate) {
+        constructor(tasksService:services.TasksService,
+                    $log:ng.ILogService,
+                    $q:ng.IQService,
+                    $ionicTabsDelegate,
+                    $ionicLoading) {
+
             this.tasksService = tasksService;
             this.$log = $log;
             this.$q = $q;
             this.$ionicTabsDelegate = $ionicTabsDelegate;
+            this.$ionicLoading = $ionicLoading;
         }
 
         addTask() {
             this.tasksService.createTask(this.content, this.due)
                 .then((result) => {
                     this.content = '';
+                    this.$ionicLoading.show({
+                        template: 'Task Added!',
+                        noBackdrop: true,
+                        duration: 2000
+                    });
                     return result;
                 })
                 .catch((error) => {
                     if (error.status === 401) {
+                        this.$ionicLoading.show({
+                            template: 'Please Log In.',
+                            noBackdrop: true,
+                            duration: 2000
+                        });
                         this.$ionicTabsDelegate.select(Tab.ACCOUNT);
                     } else {
 
